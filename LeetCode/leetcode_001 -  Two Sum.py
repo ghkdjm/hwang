27af1d5cd9ed9a9a9e51c7a711d2d632a49a6827
ghkdjm <jm3274524@naver.com>
2024-06-28 17:01:52 +0900

    Update leetcode_001 -  Two Sum.py

--- LeetCode/leetcode_001 -  Two Sum.py
+++ LeetCode/leetcode_001 -  Two Sum.py
@@ -7,6 +7,9 @@ class Solution:
                     return [i, nums.index(a)]
 
 #Description
-#Given an integer x, return true if x is a palindrome, and false otherwise.
+#Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+#You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#You can return the answer in any order.
 
-#nums를 1번 순회하며 합이 target이 되는 두 수의 index를 찾음
+#Solution
+#nums를 1번 순회하며 합이 target이 되는 두 수의 index를 찾아 반환함
