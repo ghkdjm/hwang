0b5c7fd321c977300911697a4b8a271f5aca471f
ghkdjm <jm3274524@naver.com>
2024-06-29 19:03:36 +0900

    Update leetcode_009 - Palindrome_Number.py

--- LeetCode/Easy/leetcode_009 - Palindrome_Number.py
+++ LeetCode/Easy/leetcode_009 - Palindrome_Number.py
@@ -6,8 +6,8 @@ class Solution:
         return False
 
 #Description
-#Given an integer x, return true if x is a palindrome, and false otherwise.
+'''Given an integer x, return true if x is a palindrome, and false otherwise.'''
 
 #Solution
-#int형 자료를 str형 자료로 변환한 후 슬라이싱을 통해 x를 거꾸로 배열한 것과 x를 비교함
-#같으면 True, 다르면 False를 반환함
+'''int형 자료를 str형 자료로 변환한 후 슬라이싱을 통해 x를 거꾸로 배열한 것과 x를 비교함
+같으면 True, 다르면 False를 반환함'''
