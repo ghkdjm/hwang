75bbe3a479df9b952000393c67721aecab1bb889
ghkdjm <jm3274524@naver.com>
2024-06-30 00:44:12 +0900

    Update leetcode_0202 - Happy_Number.py

--- LeetCode/Easy/leetcode_0202 - Happy_Number.py
+++ LeetCode/Easy/leetcode_0202 - Happy_Number.py
@@ -13,3 +13,19 @@ class Solution:
                 end.add(sum)
             n = sum
         return True
+
+#Description
+'''
+Write an algorithm to determine if a number n is happy.
+A happy number is a number defined by the following process:
+-Starting with any positive integer, replace the number by the sum of the squares of its digits.
+-Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+-Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+'''
+
+#Solution
+'''
+주어진 n에 대하여 주어진 계산을 진행하다보면 Happy Number가 아닌 경우에는 항상 똑같은 수가 반복되어 나옴
+이미 계산되어 나온 수를 end에 추가하고 계속 계산을 하다가 end에 그 수가 존재한다면 False를 반환함
+'''
