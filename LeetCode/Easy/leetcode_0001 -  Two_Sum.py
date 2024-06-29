51e8e70f08b309a48ad36fe1edcd29cdbd2edd99
ghkdjm <jm3274524@naver.com>
2024-06-29 19:57:56 +0900

    Update leetcode_0001 -  Two_Sum.py

--- LeetCode/Easy/leetcode_0001 -  Two_Sum.py
+++ LeetCode/Easy/leetcode_0001 -  Two_Sum.py
@@ -7,11 +7,15 @@ class Solution:
                     return [i, nums.index(a)]
 
 #Description
-'''Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
-You can return the answer in any order.'''
+You can return the answer in any order.
+'''
 
 #Solution
-'''nums를 1번 순회하며 합이 target이 되는 두 수의 index를 찾아 반환함
+'''
+nums를 1번 순회하며 합이 target이 되는 두 수의 index를 찾아 반환함
 target - nums[i]가 nums에 있어도 그 수가 nums[i]와 같은 수일 수 있으므로 6번째 줄의 조건도 필요함
-ex)target = 6, nums = [3, 2, 4] --> 3 + 3 = 6'''
+ex)target = 6, nums = [3, 2, 4] --> 3 + 3 = 6
+'''
