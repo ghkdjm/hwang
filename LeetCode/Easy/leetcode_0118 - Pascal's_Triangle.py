fd6d2ec19d044f60475765436a49251ebfbc947b
ghkdjm <jm3274524@naver.com>
2024-06-30 18:08:49 +0900

    Update leetcode_0118 - Pascal's_Triangle.py

--- LeetCode/Easy/leetcode_0118 - Pascal's_Triangle.py
+++ LeetCode/Easy/leetcode_0118 - Pascal's_Triangle.py
@@ -20,6 +20,6 @@ In Pascal's triangle, each number is the sum of the two numbers directly above i
 #Solution
 '''
 파스칼의 삼각형에서 한 행이 주어졌을 때 다음 행을 구하여 반환하는 함수를 정의함
-주어진 행의 두 항의 합(lst[i] + lst[i + 1])을 순서대로 [1]에 추가하고 마지막에 또 1을 추가함
+주어진 list의 두 항의 합(lst[i] + lst[i + 1])을 순서대로 [1]에 추가하고 마지막에 또 1을 추가함
 [[1]]이라는 리스트를 만들고 주어진 numRows만큼 정의한 함수를 통해 얻은 list를 [[1]]에 추가하여 반환함
 '''
