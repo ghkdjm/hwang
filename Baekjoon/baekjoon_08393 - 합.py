c0e581350621d6f6c93ff46195a885ad0ab2300f
ghkdjm <jm3274524@naver.com>
2024-07-16 20:13:37 +0900

    Update baekjoon_08393 - 합.py

--- Baekjoon/baekjoon_08393 - 합.py
+++ Baekjoon/baekjoon_08393 - 합.py
@@ -23,31 +23,23 @@ for 문을 사용하여 0에 1부터 n까지 더한 후 그 값을 출력함
 '''
 
 #while 문 사용
-'''
 n = int(input())
 result = 0
 while n:
     result += n
     n -= 1
 print(result)
-'''
 
 #1부터 n까지의 합 공식 사용
-'''
 n = int(input())
 print((n * (n+1))//2)
-'''
 
 #sum 함수 사용
-'''
 print(sum(range(1, (n := int(input()))+1)))
-'''
 
 #재귀 함수 사용
-'''
 def sum_recursive(n):
     if n == 1: return 1
     else: return n + sum_recursive(n-1)
 
 print(sum_recursive(n := int(input())))
-'''
