cc1b0ed0f22160cfe771bb804b5adf4b1ebd84ec
ghkdjm <jm3274524@naver.com>
2024-08-05 15:11:37 +0900

    Update leetcode_0009 - Palindrome_Number.py

--- LeetCode/Easy/leetcode_0009 - Palindrome_Number.py
+++ LeetCode/Easy/leetcode_0009 - Palindrome_Number.py
@@ -13,3 +13,12 @@ Given an integer x, return true if x is a palindrome, and false otherwise.
 int형 자료를 str형 자료로 변환한 후 슬라이싱을 통해 x를 거꾸로 배열한 것과 x를 비교함
 같으면 True, 다르면 False를 반환함
 '''
+
+#int형 자료형을 str형 자료형으로 바꾸지 않고 하는 방법
+class Solution:
+    def isPalindrome(x: int) -> bool:
+        num = []
+        while x:
+            num.append(x % 10)
+            x //= 10
+        return num == reversed(num)
