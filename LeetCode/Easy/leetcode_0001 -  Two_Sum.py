84ef06ebdd1ebf1ce8ba5bec0cf3c7038197c958
ghkdjm <jm3274524@naver.com>
2024-08-05 15:13:17 +0900

    Update leetcode_0001 -  Two_Sum.py

--- LeetCode/Easy/leetcode_0001 -  Two_Sum.py
+++ LeetCode/Easy/leetcode_0001 -  Two_Sum.py
@@ -19,3 +19,12 @@ nums를 1번 순회하며 합이 target이 되는 두 수의 index를 찾아 반
 target - nums[i]가 nums에 있어도 그 수가 nums[i]와 같은 수일 수 있으므로 6번째 줄의 조건도 필요함
 ex)target = 6, nums = [3, 2, 4] --> 3 + 3 = 6
 '''
+
+#정수값:인덱스로 dict를 만들어 답 구하는 방법
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = {}
+        for i in range(len(nums)):
+            val = target - nums[i]
+            if val not in dic: dic[nums[i]] = i
+            else: return [i, dic[val]]
